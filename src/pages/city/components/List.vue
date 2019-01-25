1c4a9fc47d0ef6293be48d2b92cb7cb4c0f6fe2c
bboyAyao <13526820201@sina.cn>
2019-01-25 12:02:59 +0800

    ajax city

--- src/pages/city/components/List.vue
+++ src/pages/city/components/List.vue
@@ -12,38 +12,18 @@
       <div class="area">
         <div class="title border-topbottom">热门城市</div>
         <div class="button-list">
-          <div class="button-wrapper">
-            <div class="button">北京</div>
+          <div
+            class="button-wrapper"
+            v-for="item of hot"
+            :key="item.id">
+            <div class="button">{{item.name}}</div>
           </div>
         </div>
       </div>
-      <div class="area">
-        <div class="title border-topbottom">A</div>
-        <div class="item-list">
-           <div class="item border-bottom">阿尔拉</div>
-           <div class="item border-bottom">阿尔拉</div>
-           <div class="item border-bottom">阿尔拉</div>
-           <div class="item border-bottom">阿尔拉</div>
-           <div class="item border-bottom">阿尔拉</div>
-           <div class="item border-bottom">阿尔拉</div>
-        </div>
-        <div class="title border-topbottom">A</div>
-        <div class="item-list">
-           <div class="item border-bottom">阿尔拉</div>
-           <div class="item border-bottom">阿尔拉</div>
-           <div class="item border-bottom">阿尔拉</div>
-           <div class="item border-bottom">阿尔拉</div>
-           <div class="item border-bottom">阿尔拉</div>
-           <div class="item border-bottom">阿尔拉</div>
-        </div>
-        <div class="title border-topbottom">A</div>
-        <div class="item-list">
-           <div class="item border-bottom">阿尔拉</div>
-           <div class="item border-bottom">阿尔拉</div>
-           <div class="item border-bottom">阿尔拉</div>
-           <div class="item border-bottom">阿尔拉</div>
-           <div class="item border-bottom">阿尔拉</div>
-           <div class="item border-bottom">阿尔拉</div>
+      <div class="area" v-for="(items,key) of cities" :key="key">
+        <div class="title border-topbottom">{{key}}</div>
+        <div class="item-list" v-for="item of items" :key="item.id">
+           <div class="item border-bottom">{{item.name}}</div>
         </div>
       </div>
     </div>
@@ -54,6 +34,10 @@
 import Bscroll from 'better-scroll'
 export default {
   name: 'CityList',
+  props: {
+    hot: Array,
+    cities: Object
+  },
   mounted () {
     this.scroll = new Bscroll(this.$refs.wrapper)
   }
